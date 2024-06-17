5e04793e220f2acaf6af01bd4251cd3db8388b1f
Niroshan <niroshan9812@gmail.com>
2024-06-17 11:02:27 +0530

    Authentication proces initial stage currection

---
+++ app/_components/Exmp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+
+function exmp() {
+  return (
+  
+    <dev>
+       <h1>The main context</h1>
+    </dev>
+  );
+}
+
+export default exmp;
