a0faa403da2ca6647e157b0a783b3296e84b6691
Niroshan <niroshan9812@gmail.com>
2024-06-19 19:15:34 +0530

    finalized db Confugaration

--- configs/schema.js
+++ configs/schema.js
@@ -1,10 +1,11 @@
 // Create databace table 
 
-import { pgTable } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
 
-export const FsonFoms = pgTable('FsonFoms',{
-    id:serial('id').primaryKey(),
-    jsonform:text ('jsonform').notNull(),
-    createdBy:varchar('createdBy').notNull(),
-    createdAt:varchar('createdAt').notNull()
-})
+
+export const FsonFoms = pgTable('FsonFoms', {
+    id: serial('id').primaryKey(),
+    jsonform: text('jsonform').notNull(),
+    createdBy: varchar('createdBy').notNull(),
+    createdAt: varchar('createdAt').notNull()
+});
--- drizzle.config.js
+++ drizzle.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "drizzle-kit";
- 
+
 export default defineConfig({
-  schema: "/configs/schema.js",
+  schema: "./configs/schema.js",
   out: "./drizzle",
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.NEXT_PUBLIC_DATABACE_URL_CONFIG,
+   url: 'postgresql://AiFormGen_owner:ciJW1OBbjhM7@ep-dry-cherry-a5vf2nnu.us-east-2.aws.neon.tech/AiFormGen?sslmode=require',
+   
+    
   }
 });
