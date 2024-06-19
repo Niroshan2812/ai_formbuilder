e6f763f88c699aff2777fbe6d46b54ba71dee524
Niroshan <niroshan9812@gmail.com>
2024-06-19 15:29:55 +0530

    Databace Confuguration stage 1

--- configs/schema.js
+++ configs/schema.js
@@ -1,2 +1,10 @@
 // Create databace table 
 
+import { pgTable } from "drizzle-orm/pg-core";
+
+export const FsonFoms = pgTable('FsonFoms',{
+    id:serial('id').primaryKey(),
+    jsonform:text ('jsonform').notNull(),
+    createdBy:varchar('createdBy').notNull(),
+    createdAt:varchar('createdAt').notNull()
+})
