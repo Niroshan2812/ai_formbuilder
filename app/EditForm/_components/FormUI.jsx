aa3da09e7bca35bde730f16a32a94db983a96034
Niroshan <niroshan9812@gmail.com>
2024-06-25 16:17:20 +0530

    form Ui intergration

--- app/EditForm/_components/FormUI.jsx
+++ app/EditForm/_components/FormUI.jsx
@@ -5,6 +5,21 @@ function FormUI({ jsonForm }) {
     <div className="border p-5">
       <h2 className="font-bold text-center text-2xl text-sky-700">{jsonForm?.formTitle}</h2>
       <h2 className="text-sm text-gray-400 text-center">{jsonForm?.formSubheading}</h2>
+
+      {jsonForm?.formFields?.length > 0 ? (
+        jsonForm.formFields.map((field, index) => (
+          <div key={index}>
+            <input
+              type={field.fieldType}
+              placeholder={field.placeholder}
+              name={field.fieldName}
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+        ))
+      ) : (
+        <p>No form fields available</p>
+      )}
     </div>
   );
 }
